39da875f51131bc38ab12e02e5b4750c1de81ce1
atthoriqi <thoriq1997@gmail.com>
2016-07-22 09:59:38 +0700

    coba

--- testing_git.php
+++ testing_git.php
@@ -1,6 +1,6 @@
 <?php
     echo  "thoriq kau memang keren...ganteng..kalian diam.. no comment"
-    
+    echo "jauh dimata dekat di hati"
 	echo "12312321312321";
 
 	echo "saipul hidayat The'gerr";
