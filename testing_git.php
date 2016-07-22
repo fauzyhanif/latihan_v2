f1caad9425f50843bff1bf95acf54adf5580d2db
atthoriqi <thoriq1997@gmail.com>
2016-07-22 10:25:00 +0700

    Edisi Jones

--- testing_git.php
+++ testing_git.php
@@ -1,13 +1,23 @@
 <?php
     echo  "thoriq kau memang keren...ganteng..kalian diam.. no comment"
+
     echo "jauh dimata dekat di hati"
+
     echo "status FB Zaid Hummam: BIDADARI dunia memang tak bersayap tapi ia pasti BERHIJAB
           semoga itu kamu...muslimah";
-    echo "laper...piye iki..?";      
+
+    echo "status FB hummam: Berbuat baiklah kepada wanita..karena sesungguhnya mereka di ciptakan dari tulang rusuk, dan sesungguhnya tulang rusuk yang paling bengkok adalah yang paling atas maka sikapilah para wanita dengan baik.(HR.Bukhori no.5186)
+‪        #‎smogabermanpaat‬";
+
+    echo "status JoNes..Edisi humman kebelet nikah";  
+
+    echo "laper...piye iki..?"; 
+
 	echo "12312321312321";
 
 	echo "saipul hidayat The'gerr";
 	
 	echo "Abdurrahman";
+	
 	echo "NaufallutfiF";
 ?>
