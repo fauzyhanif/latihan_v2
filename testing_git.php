0932898c157ac6051e6c7e19f0edd5a7157e48c8
atthoriqi <thoriq1997@gmail.com>
2016-07-22 10:04:29 +0700

    coba

--- testing_git.php
+++ testing_git.php
@@ -1,6 +1,8 @@
 <?php
     echo  "thoriq kau memang keren...ganteng..kalian diam.. no comment"
     echo "jauh dimata dekat di hati"
+    echo "status FB Zaid Hummam: BIDADARI dunia memang tak bersayap tapi ia pasti BERHIJAB
+          semoga itu kamu...muslimah";
 	echo "12312321312321";
 
 	echo "saipul hidayat The'gerr";
